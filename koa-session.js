1fd1b49ab6e20dc8b7b0b50a0da1acc2eb583f28
1215904405a <1215904405@qq.com>
2017-11-07 03:29:28 -0600

    Update koa-session.js

--- koa-session.js
+++ koa-session.js
@@ -22,7 +22,9 @@ const CONFIG = {
   /** Warning: If a session cookie is stolen, this cookie will never expire */
   maxAge: 86400000,
   overwrite: true, /** (boolean) can overwrite or not (default true)  cookie能否被覆盖*/
-  httpOnly: true, /** (boolean) httpOnly or not (default true)  能否被document.cookie读取防止跨脚本攻击*/
+  httpOnly: true, /** (boolean) httpOnly or not (default true)  能否被document.cookie读取防止跨脚本攻击   Set-Cookie: =[; =]
+[; expires=][; domain=]
+[; path=][; secure][; HttpOnly]*/
   signed: true, /** (boolean) signed or not (default true) */
   rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. default is false **/
 };
