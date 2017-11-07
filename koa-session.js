09128404a107211bed86efa5c1597edcedc2cc07
1215904405a <1215904405@qq.com>
2017-11-07 03:17:01 -0600

    session config

--- koa-session.js
+++ koa-session.js
@@ -10,3 +10,21 @@
  let sig1 = crypto.createHmac('sha1', 'P@LW9XXA@O10ZrXj~!]/mNHH').update('X=' + encode({ "providerId": "1000754" })).digest('base64').replace(/\/|\+|=/g, function(x) {
    return ({ "/": "_", "+": "-", "=": "" })[x]
  });
+
+
+
+app.keys = ['some secret hurr'];
+
+const CONFIG = {
+  key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+  /** (number || 'session') maxAge in ms (default is 1 days) */
+  /** 'session' will result in a cookie that expires when session/browser is closed */
+  /** Warning: If a session cookie is stolen, this cookie will never expire */
+  maxAge: 86400000,
+  overwrite: true, /** (boolean) can overwrite or not (default true)  cookie能否被覆盖*/
+  httpOnly: true, /** (boolean) httpOnly or not (default true)  能否被document.cookie读取防止跨脚本攻击*/
+  signed: true, /** (boolean) signed or not (default true) */
+  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. default is false **/
+};
+
+app.use(session(CONFIG, app));
