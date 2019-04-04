b9e8e3757eeed1cbab88da3e61089e709bf5a5c8
1215904405a <1215904405@qq.com>
2019-04-04 10:33:28 +0800

    Update getPath.js

--- getPath.js
+++ getPath.js
@@ -1,6 +1,8 @@
 
 process.cwd() 当前执行程序的路径（执行命令行时候的路径,不是代码路径 例如 在根目录下执行 node ./xxx/xxx/a.js 则 cwd 返回的是 根目录地址 ）
 
-__dirname: 代码存放的位置
+__dirname: 代码存放的位置(文件夹)
 
 process.execPath: 当前执行的node路径（如：/bin/node）
+
+__filename: 返回当前模块文件被解析过后的绝对路径,
